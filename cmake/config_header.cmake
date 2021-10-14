6a5a25b312c17af01db124fc71c2c063ff11bd5d
Jerry Ma <jerry.ma.nk@gmail.com>
2021-10-14 11:58:50 -0400

    fix mistake

--- cmake/config_header.cmake
+++ cmake/config_header.cmake
@@ -59,3 +59,17 @@ function(generate_gitversion_header name)
     )
     add_dependencies(tula_config_header gitversion_header_${name})
 endfunction()
+
+# Config header
+function(generate_config_header name)
+    set(config_header_srcdir ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src)
+    get_config_header_output_dir(output_dir ${name})
+    set(output_path ${output_dir}/config.h)
+    verbose_message("Generate config header ${output_path}")
+    string(TOUPPER ${name} _build_prefix)
+    configure_file(${config_header_srcdir}/config.h.in ${output_path} @ONLY)
+    add_custom_target(config_header_${name} DEPENDS
+        ${output_path}
+    )
+    add_dependencies(tula_config_header config_header_${name})
+endfunction()
