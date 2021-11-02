5c7f86285ec3ed5ec9c68c044e6ad28ab7963968
toltec <toltec@umass.edu>
2021-11-02 15:40:22 +0000

    allow use installed ccfits because conan ccfits has issues on unity

--- cmake/CCfits.cmake
+++ cmake/CCfits.cmake
@@ -10,13 +10,40 @@ set(ccfits_libs "")
 
 if (USE_INSTALLED_CCFITS)
     verbose_message("Use system installed CCfits.")
-    find_package(CCfits REQUIRED CONFIG)
+    # find_package(CCfits REQUIRED CONFIG)
+    # set(ccfits_libs ${ccfits_libs} CCfits::CCfits)
+    message(STATUS "search CCfits in ${CCFITS_PREFIX}")
+    find_path(CCFITS_INCLUDE_DIR CCfits/CCfits
+        PATHS ${CCFITS_PREFIX}/include
+        )
+    find_library(CCFITS_LIBRARY CCfits
+        PATHS ${CCFITS_PREFIX}/lib
+        )
+    find_library(CFITSIO_LIBRARY cfitsio
+        PATHS ${CCFITS_PREFIX}/lib
+        )
+    set(CCFITS_FOUND false)
+    if (CCFITS_INCLUDE_DIR AND CCFITS_LIBRARY)
+        set(CCFITS_FOUND true)
+    endif()
+    MESSAGE(STATUS "CCfits include: ${CCFITS_INCLUDE_DIR}")
+    MESSAGE(STATUS "CCfits lib: ${CCFITS_LIBRARY}")
+    if (CCFITS_FOUND)
+        MESSAGE(STATUS "Found CCfits: ${CCFITS_LIBRARY}")
+    else()
+        MESSAGE(FATAL_ERROR "Cannot find CCfits")
+    endif()
+    add_library(ccfits INTERFACE)
+    target_include_directories(ccfits INTERFACE ${CCFITS_INCLUDE_DIR})
+    target_link_libraries(ccfits INTERFACE ${CCFITS_LIBRARY} ${CFITSIO_LIBRARY} z)
+    add_library(CCfits::CCfits ALIAS ccfits)
     set(ccfits_libs ${ccfits_libs} CCfits::CCfits)
 else()
     if (CONAN_INSTALL_CCFITS)
         include(conan_helper)
         ConanHelper(REQUIRES
             ccfits/[>=2.6]
+            BUILD all
             )
         find_package(ccfits REQUIRED MODULE)
         verbose_message("Use conan installed CCfits")
